14c5871de713a91bc45eaca57a154a3b35a4f85d
DeepakAang08 <vengatesandeepak88@gmail.com>
2026-04-29 18:12:34 +0800

    Update bunkr_bot.py

--- bunkr_bot.py
+++ bunkr_bot.py
@@ -8,7 +8,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 # ============================
 # PUT YOUR BOT TOKEN HERE
 # ============================
-BOT_TOKEN = "8604002829:AAGFJfeWqkECk_og90dsLFFyE6wsa1AVbfA"
+BOT_TOKEN = "8604002829:AAFGYbn3Hx7FyXU1MoXdYToEpZzqTdoJ55A"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
